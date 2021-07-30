037a8fd6b3d32103855ce02152c113fb8798ab75
mrhydejc <julien.coutand@arxcf.com>
2021-07-30 12:03:30 +0200

    testU

--- pokerOnline/src/test/java/com/arx/poker/GameTest.java
+++ pokerOnline/src/test/java/com/arx/poker/GameTest.java
@@ -11,13 +11,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.arx.poker.model.Card;
 import com.arx.poker.model.GameState;
+import com.arx.poker.model.GameStateStatusEnum;
+import com.arx.poker.model.PhaseEnum;
 import com.arx.poker.model.Player;
+import com.arx.poker.service.ActionEnum;
+import com.arx.poker.service.GameHolderService;
 import com.arx.poker.service.GameService;
 
 @SpringBootTest
 public class GameTest {
 	@Autowired
 	private GameService gameService;
+	@Autowired
+	private GameHolderService gameHolderService;
 	
 	@Test
 	public void should_pick_random_first_player() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException 
@@ -89,64 +95,24 @@ public class GameTest {
 		Assertions.assertEquals(0, intersection.size());
 	}
 	
-//	@Test
-//	public void should_set_next_player() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException 
-//	{
-//		GameState gs = new GameState();
-//		gameService.initDeck(gs);
-//		Player p1 = new Player("player1");
-//		Player p2 = new Player("player2");
-//		Player p3 = new Player("player3");
-//		Player p4 = new Player("player4");
-//		gameService.registerPlayer(p1, gs);
-//		gameService.registerPlayer(p2, gs);
-//		gameService.registerPlayer(p3, gs);
-//		gameService.registerPlayer(p4, gs);
-//
-//		gs.getPlayersInGame().addAll(gs.getPlayers());
-//		gs.setDealer(p1);
-//		gameService.betBlinds(gs);
-//		gs.setCurrentPlayer(p3);
-//		
-//		Player nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertEquals(p4.getName(),nextPlayer.getName());
-//		
-//		//p4 suit
-//		p4.setBet(2);
-//		gs.setCurrentPlayer(p4);
-//		
-//		nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertEquals(p1.getName(),nextPlayer.getName());
-//		
-//		//p1 se couche
-//		gs.getPlayersInGame().remove(p1);
-//		gs.setCurrentPlayer(p1);
-//		
-//		nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertEquals(p2.getName(),nextPlayer.getName());
-//		
-//		//p2 relance
-//		p2.setBet(4);
-//		gs.setCurrentPlayer(p2);
-//		
-//		nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertEquals(p3.getName(),nextPlayer.getName());
-//		
-//		//p3 suit
-//		p3.setBet(4);
-//		gs.setCurrentPlayer(p3);
-//		
-//		nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertEquals(p4.getName(),nextPlayer.getName());
-//		
-//		//p4 suit
-//		p4.setBet(4);
-//		gs.setCurrentPlayer(p4);
-//		
-//		nextPlayer= gameService.nextPlayer(gs);
-//		Assertions.assertNull(nextPlayer);
-//	}
-	
+	@Test
+	public void test_fold_should_end_round() {
+		int id = gameService.createGame(2).getId();
+		
+		int playerId1 = gameService.addPlayerToSpecificGame(id, "test1").getPlayerId();
+		int playerId2 = gameService.addPlayerToSpecificGame(id, "test2").getPlayerId();
+		
+		Assertions.assertEquals(GameStateStatusEnum.IN_PROGRESS, gameService.getGame(id, playerId1).getStatus());
+		Assertions.assertEquals(PhaseEnum.PRE_FLOP, gameService.getGame(id, playerId1).getPhase());
+		Assertions.assertEquals(1, gameService.getGame(id, playerId1).getCurrentRound());
+		
+		GameState gs = gameHolderService.accessSpecificGameState(id);
+		gameService.reactToPlayerAction(id, gs.getCurrentPlayer().getId(), ActionEnum.FOLD);
+		
+		Assertions.assertEquals(GameStateStatusEnum.IN_PROGRESS, gameService.getGame(id, playerId1).getStatus());
+		Assertions.assertEquals(PhaseEnum.PRE_FLOP, gameService.getGame(id, playerId1).getPhase());
+		Assertions.assertEquals(2, gameService.getGame(id, playerId1).getCurrentRound());
+	}
 	
 	
 	
